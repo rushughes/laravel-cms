aafb6d354f052b166e2d05a29e544b42b1522205
Rus Hughes <russell.hughes@gmail.com>
2019-09-29 07:50:20 +0200

    s10 l65

--- app/User.php
+++ app/User.php
@@ -38,6 +38,10 @@ class User extends Authenticatable
     ];
 
     public function post() {
+      return $this->hasOne('App\Post');
+    }
+
+    public function posts() {
       return $this->hasMany('App\Post');
     }
 }
